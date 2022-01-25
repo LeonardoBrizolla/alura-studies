7e1540a8d5e10b98bcdbe733dbf23758cdfd64e4
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-25 14:20:10 -0300

    Introduzindo estados

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import { Item } from './Item';
 
 import styles from './List.module.scss';
 
 function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       title: 'React',
       time: '02:00:00',
@@ -16,11 +17,17 @@ function List() {
       title: 'Styled Components',
       time: '01:30:00',
     },
-  ];
+  ]);
 
   return (
     <aside className={styles.listaTarefas}>
-      <h2>Studies of the day</h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks, { title: 'Study state', time: '01:00:00' }]);
+        }}
+      >
+        Studies of the day
+      </h2>
       <ul>
         {tasks.map((task, index) => (
           <Item key={index} {...task} />
